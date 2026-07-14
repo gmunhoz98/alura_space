cd5177d5c8d36318d33d3d8d2ca9937daea0da7c
GabrielMunhoz <gabriel.munhoz@icarotech.com>
2026-07-14 17:17:12 -0300

    Banco de dados: Imagens e filtros

--- galeria/views.py
+++ galeria/views.py
@@ -4,8 +4,9 @@ from galeria.models import Fotografia
 
 
 def index(request):
-    fotografias = Fotografia.objects.all()
-
+    fotografias = (
+        Fotografia.objects.all().order_by("-id").filter(publicada=True)
+    )
     return render(request, "galeria/index.html", {"cards": fotografias})
 
 
--- setup/settings.py
+++ setup/settings.py
@@ -123,3 +123,8 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "setup/static")]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+# Media
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+MEDIA_URL = "/media/"
