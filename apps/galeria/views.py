2f1667ff104b3c508689ea68961a32a181370f95
GabrielMunhoz <gabriel.munhoz@icarotech.com>
2026-07-19 18:30:38 -0300

    Aula 01: Melhorias no projeto

--- apps/galeria/views.py
+++ apps/galeria/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404, render, redirect
 
-from galeria.models import Fotografia
+from apps.galeria.models import Fotografia
 
 from django.contrib import messages
 
